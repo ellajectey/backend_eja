dbf414320fc4753030c7505e71ff43399b791ed2
EllaJay23 <akuaasabea23@gmail.com>
2024-03-13 00:17:46 +0000

    missed mongo db class code

--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -1,6 +1,34 @@
 import { Router } from "express";
+import { MongoClient } from "mongodb";
 
 const router = Router();
+const url ='mongodb+srv://todo-api:7cUTfwDQxEI0DLiL@todo-app-api.3icpqut.mongodb.net/?retryWrites=true&w=majority&appName=Todo-APP-APi';
+                                    
+const client = new MongoClient(url);
+
+//define routes 
+router.post('/todos', async (req,res) => {
+    //connect mongodb client
+    await client.connect();
+
+    // Get access to todo database
+    const db = client.db('todo-db');
+
+    // Get access to todos collection
+    const collection = db.collection('todos');
+
+    // Add to do document to todos collection
+    const result = await collection.insertOne(req.body);
+
+    // Disconnect mongodb client
+    await client.close();
+
+    // Return response
+    res.json(result);
+});
+
+
+
 
 // Define routes
 router.post('/todos', (req, res) => {
